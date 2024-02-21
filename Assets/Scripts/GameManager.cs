a2fcffb47c8933930f9640c3fb7ff2f1502bf5a1
Safirl <loic.leforestier@mmibordeaux.com>
2024-02-21 22:19:07 +0100

    Starting from 0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,129 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int fingers;
-    [SerializeField] int score;
-    const int WIN_SCORE = 5;
-    [SerializeField] SequenceManager sequenceManager;
-    [SerializeField] public GameObject boss;
-    [SerializeField] public Animator bossAnimator;
-    [SerializeField] public GameObject timer;
-    [SerializeField] public GameObject choiceContainer;
-    [SerializeField] public GameObject issueContainer;
 
+    [SerializeField] private int lives;
+    [SerializeField] private int sequenceNumber = 0;
+    [SerializeField] private UnityEvent loseGame;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-        if (sequenceManager == null)
-        {
-            sequenceManager = GameObject.FindGameObjectWithTag("SequenceManager").GetComponent<SequenceManager>();
-        }
-        if (boss == null || bossAnimator == null)
-        {
-            boss = GameObject.FindGameObjectWithTag("Boss");
-            bossAnimator = boss.GetComponent<Animator>();
-        }
-        if (timer == null)
-        {
-            timer = GameObject.FindGameObjectWithTag("Timer");
-        }
-        StartCoroutine(Init());
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-           
+        
     }
 
-    public void WinSequence()
+    void RemoveLife()
     {
-        score++;
-        if (score == WIN_SCORE) 
+        lives -= 1;
+    }
+
+    
+    void CheckGameState()
+    {
+        if (lives < 1)
         {
-            StartCoroutine(Happy());
+            loseGame.Invoke();
         }
-        //Appelle WinScript de fin
-        else 
-        {
-            StartCoroutine(Satisfy());
-        }
-    }
-
-    public void LoseSequence()
-    {
-        fingers--;
-        if (fingers == 0) 
-        {
-            StartCoroutine(Angry());
-            //loseScript
-        }
-        else
-            StartCoroutine(Frappe());
-    }
-
-    public void StartSequence()
-    {
-        sequenceManager.SetCurrentSequence(score);
-        StartCoroutine(Init());
-    }
-
-    IEnumerator Init()
-    {
-        //choiceContainer = GameObject.FindGameObjectWithTag("ChoiceContainer");
-        choiceContainer.SetActive(false);
-        //issueContainer = GameObject.FindGameObjectWithTag("IssueContainer");
-        issueContainer.SetActive(true);
-
-        float animationTime = 2f;
-        timer.GetComponent<TimerBehavior>().thinkTime = animationTime;
-        sequenceManager.SetTimer(animationTime);
-        yield return new WaitForSeconds(animationTime);
-
-        //Logique
-        choiceContainer.SetActive(true);
-        issueContainer.SetActive(false);
-        sequenceManager.SetIssueText();
-        sequenceManager.SetButtonsChoices(sequenceManager.GetCurrentSequence().GetComponent<Sequence>().GetFirstChoices());
-
-    }
-
-    IEnumerator Satisfy() //quand tu gagnes une séquence
-    {
-        boss.GetComponent<Animator>().SetBool("boss_satisfy", true);
-        yield return new WaitForSeconds(2.14f);
-        boss.GetComponent<Animator>().SetBool("boss_satisfy", false);
-
-        StartSequence();
-
-    }
-
-    IEnumerator Frappe()
-    {
-        boss.GetComponent<Animator>().SetBool("boss_frappe", true);
-        yield return new WaitForSeconds(2.22f);
-        boss.GetComponent<Animator>().SetBool("boss_frappe", false);
-        StartSequence();
-
-    }
-    IEnumerator Happy()
-    {
-        boss.GetComponent<Animator>().SetBool("boss_happy", true);
-        yield return new WaitForSeconds(6.0f);
-        boss.GetComponent<Animator>().SetBool("boss_happy", false);
-    }
-
-    IEnumerator Angry()
-    {
-        boss.GetComponent<Animator>().SetBool("boss_angry", true);
-        yield return new WaitForSeconds(1.02f);
-        boss.GetComponent<Animator>().SetBool("boss_angry", false);
     }
 }
--- Assets/Scripts/SequenceManager.cs
+++ Assets/Scripts/SequenceManager.cs
@@ -1,148 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SequenceManager : MonoBehaviour
 {
-    [SerializeField] GameObject currentSequence;
-    [SerializeField] GameObject issue;
-    [SerializeField] GameObject[] sequences;
-    [SerializeField] GameObject[] buttons;
-    [SerializeField] GameObject[] playerResponses;
-    [SerializeField] GameObject defaultAnswer;
-    [SerializeField] float timer;
-
-
-
-    [SerializeField] GameManager gameManager;
-
-    public float GetTimer()
-    {
-        return timer;
-    }
-
-    public GameObject GetCurrentSequence()
-    {
-        return currentSequence;
-    }
-    public void SetCurrentSequence(int sequenceNumber)
-    {
-        currentSequence = sequences[sequenceNumber];
-    }
-
-    public void SetTimer(float newTimer)
-    {
-        timer = newTimer;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-
-        if (gameManager == null)
-        {
-            gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-        }
-        if (currentSequence == null)
-        {
-            currentSequence = sequences[0];
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
-        {
-            //CancelChoice();
-        }
-    }
-
-    public void AddChoiceToPlayerResponses(GameObject choice)
-    {
-        List<GameObject> listeResponses = new List<GameObject>(playerResponses);
-
-        // Ajouter un élément à la liste
-        listeResponses.Add(choice);
-
-        // Reconvertir la liste en tableau
-        playerResponses = listeResponses.ToArray();
-    }
-
-    public void CheckIfResponseIsComplete(GameObject choice)
-    {
-        if (playerResponses.Length == 3)
-        {
-            CheckChoices();
-            Debug.Log("CheckChoices");
-        }
-        else
-        {
-            SetButtonsChoices(choice.GetComponent<Choice>().GetChoices());
-        }
-
-    }
-
-    public void CheckChoices()
-    {
-        int badAnswers = 0;
-        for (int i = 0; i < playerResponses.Length; i++)
-        {
-            if (playerResponses[i] != currentSequence.GetComponent<Sequence>().GetValidChoices()[i])
-            {
-                badAnswers++;
-            }
-        }
-
-        if (badAnswers > 1)
-        {
-            gameManager.LoseSequence();
-            Debug.Log("loseSequence");
-        }
-        else
-            gameManager.WinSequence();
-
-        playerResponses = new GameObject[0];
-    }
-
-    public void SetIssueText()
-    {
-        string text = currentSequence.GetComponent<Sequence>().GetIssue();
-        issue.GetComponentInChildren<TMP_Text>().text = text;
-
-    }
-    public void SetButtonsChoices(GameObject[] choices)
-    {
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            ButtonBehavior buttonBehavior = buttons[i].GetComponent<ButtonBehavior>();
-            if (buttonBehavior != null)
-            {
-                buttonBehavior.SetChoice(choices[i]);
-                StartCoroutine(SetTimer());
-            }
-            else
-                Debug.Log("ButtonBehaviorNotFound");
-           
-        }
-    }
-
-    IEnumerator SetTimer()
-    {
-        float animationTime = 10f;
-        gameManager.timer.GetComponent<TimerBehavior>().thinkTime = animationTime;
-        timer = animationTime;
-        yield return new WaitForSeconds(animationTime);
-    }
-
-    void CancelChoice()
-    {
-        //defaultAnswer.GetComponent<ButtonBehavior>().ChoiceSelected();
+        
     }
 }
