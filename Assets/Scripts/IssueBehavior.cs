5f5a62eb0dbc7930fd2e76c67959b71d4c478af9
Safirl <loic.leforestier@mmibordeaux.com>
2024-12-25 15:57:24 +0100

    adding dialogues clip for sequences

--- Assets/Scripts/IssueBehavior.cs
+++ Assets/Scripts/IssueBehavior.cs
@@ -7,6 +7,7 @@ public class IssueBehavior : MonoBehaviour
 {
     [SerializeField] private float timer;
     [SerializeField] private string[] issueTexts;
+    [SerializeField] private AudioClip[] dialogues;
     [SerializeField] private GameObject defaultChoice;
     [SerializeField] private GameObject[] choices;
     [SerializeField] private GameObject[] goodChoices;
