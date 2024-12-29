7dbbaa9c318c0b184b879e85a61e0ae0193691a4
Safirl <loic.leforestier@mmibordeaux.com>
2024-12-29 11:58:47 +0100

    Update transitions and home screen animations

--- Assets/Scripts/HomePage/HomeScreenManager.cs
+++ Assets/Scripts/HomePage/HomeScreenManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -6,14 +7,41 @@ namespace HomePage
 {
     public class HomeScreenManager : MonoBehaviour
     {
+        [SerializeField] private GameObject blackScreen;
+
+        private void Start()
+        {
+            if (!blackScreen)
+            {
+                Debug.LogError("blackscreen is not assigned");
+                return;
+            }
+            blackScreen.GetComponent<Transition.Transition>().onAnimationOver.AddListener(() =>
+            {
+                SceneManager.LoadScene("GameScene");
+            });
+            blackScreen.SetActive(false);
+        }
+
         public void StartGame()
         {
-            SceneManager.LoadScene("GameScene");
+            if (!blackScreen)
+            {
+                Debug.LogError("blackscreen is not assigned");
+                return;
+            }
+            blackScreen.SetActive(true);
+            blackScreen.GetComponent<Transition.Transition>().FadeOut();
         }
         
         public void ExitGame()
         {
             Application.Quit();
+        }
+
+        private void OnDestroy()
+        {
+            blackScreen.GetComponent<Transition.Transition>().onAnimationOver.RemoveAllListeners();
         }
     }
 }
--- Assets/Scripts/Transition/Transition.cs
+++ Assets/Scripts/Transition/Transition.cs
@@ -1,13 +1,20 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Transition
 {
     public class Transition : MonoBehaviour
     {
         [SerializeField] private Animator animator;
+
+        public UnityEvent onAnimationOver;
         public void Start()
         {
+            if (!GameManager.Instance)
+            {
+                return;
+            }
             GameManager.Instance.startIntro.AddListener(FadeIn);
         }
 
@@ -20,9 +27,24 @@ namespace Transition
         {
             animator.SetTrigger("fade_in");
         }
+        
+        public void FadeOut()
+        {
+            animator.SetTrigger("fade_out");
+        }
+
+        public void OnFadeOutOver()
+        {
+            onAnimationOver.Invoke();
+            // gameObject.SetActive(false);
+        }
 
         private void OnDestroy()
         {
+            if (!GameManager.Instance)
+            {
+                return;
+            }
             GameManager.Instance.startIntro.RemoveListener(FadeIn);
         }
     }
