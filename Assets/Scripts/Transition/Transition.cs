bc1a738a76d64c2c56c55a5699a285ffb228c276
Safirl <loic.leforestier@mmibordeaux.com>
2024-12-29 10:43:50 +0100

    Add intro animation and update transition system

--- Assets/Scripts/Transition/Transition.cs
+++ Assets/Scripts/Transition/Transition.cs
@@ -1,12 +1,29 @@
+using System;
 using UnityEngine;
 
 namespace Transition
 {
     public class Transition : MonoBehaviour
     {
+        [SerializeField] private Animator animator;
+        public void Start()
+        {
+            GameManager.Instance.startIntro.AddListener(FadeIn);
+        }
+
         public void SetBlackTransitionToFalse()
         {
             gameObject.SetActive(false);
+        }
+
+        private void FadeIn()
+        {
+            animator.SetTrigger("fade_in");
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.Instance.startIntro.RemoveListener(FadeIn);
         }
     }
 }
